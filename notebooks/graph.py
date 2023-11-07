1de766b8d407c98fcd8d4e15e322a776bc21229f
mmartin4972 <mmartin4972@gmail.com>
2023-11-07 08:31:01 -0500

    Made some minor additions

---
+++ notebooks/graph.py
@@ -0,0 +1,21 @@
+class BipartiteGraph:
+    def __init__(self):
+        return
+
+    def add_data_node(self, data):
+        return
+
+    def add_keyword_node(self, keyword):
+        return
+
+    def add_edge(self, data, keyword):
+        return
+
+    def get_data(self, data):
+        return
+
+    def get_data_edges(self, data):
+        return
+
+    def get_keyword_edges(self, keyword):
+        return
