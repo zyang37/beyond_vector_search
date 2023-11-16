753b8f24f0291074cfa755aad76c5da69d5b1e26
Reggie Hsu <reggiehsu111@gmail.com>
2023-11-16 17:21:50 -0500

    add inference

---
+++ workloads/inference.py
@@ -0,0 +1,111 @@
+import pandas as pd
+import chromadb
+import argparse
+import pickle
+
+
+def create_paper_id_to_title_dict(filtered_data):
+    return pd.Series(filtered_data.title.values, index=filtered_data.id).to_dict()
+
+def create_id_to_abstract_dict(filtered_data):
+    return pd.Series(filtered_data.abstract.values, index=filtered_data.id).to_dict()
+
+
+def get_query_col(df, id2abstract_dict):
+    return df["query"]
+
+
+def get_abstract_col(df, id2abstract_dict):
+    return df["paper_id"].map(id2abstract_dict)
+
+
+def vector_search(df, coll_name, client, k, get_query_func, id2abstract_dict, batch_size=50):
+    collection = client.get_collection(name=coll_name)
+    query_col = get_query_func(df, id2abstract_dict)
+    search_results = []
+    for idx in range(0, df.shape[0], batch_size):
+        queries = query_col.iloc[idx : idx + batch_size].values.tolist()
+        results = collection.query(query_texts=queries, n_results=k)
+        search_results.extend(results["ids"])
+    return search_results
+
+
+def infer(chroma_path, workload_csv, title_col, abstract_col, id2abstract_dict, k):
+    df = pd.read_csv(workload_csv)
+    chroma_client = chromadb.PersistentClient(path=chroma_path)
+    vector_search_results = vector_search(
+        df, title_col, chroma_client, k, get_query_col, id2abstract_dict
+    )
+    ground_truths = vector_search(df, abstract_col, chroma_client, k, get_abstract_col, id2abstract_dict)
+    df[title_col] = pd.Series(vector_search_results)
+    df[abstract_col] = pd.Series(ground_truths)
+    return df
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-k",
+        type=int,
+        default=10,
+        help="number of k to retrieve for each query",
+    )
+    parser.add_argument(
+        "-c",
+        "--chroma",
+        type=str,
+        default="../data/chroma_dbs/",
+        help="path to load chroma db collections",
+    )
+    parser.add_argument(
+        "-w",
+        "--workload-csv",
+        type=str,
+        default="workload.csv",
+        help="workload csv file",
+    )
+    parser.add_argument(
+        "-a",
+        "--abstracts",
+        type=str,
+        default="abstracts",
+        help="ChromaDB collection name that stores the embeddings of abstracts",
+    )
+    parser.add_argument(
+        "-t",
+        "--titles",
+        type=str,
+        default="arxiv_vector",
+        help="ChromaDB collection name that stores the embeddings of paper titles",
+    )
+    parser.add_argument(
+        "-f",
+        "--filtered-data-path",
+        type=str,
+        default="../data/filtered_data.pickle",
+        help="path to filtered_data pickle file",
+    )
+
+    args = parser.parse_args()
+    with open(args.filtered_data_path, "rb") as f:
+        filtered_data = pickle.load(f)
+    id2title_dict = create_paper_id_to_title_dict(filtered_data)
+    for k, v in id2title_dict.items():
+        print(k, v)
+    id2abstract_dict = create_id_to_abstract_dict(filtered_data)
+    result_df = infer(
+        args.chroma,
+        args.workload_csv,
+        args.titles,
+        args.abstracts,
+        id2abstract_dict,
+        args.k,
+    )
+    for x in result_df[args.titles]:
+        print(x)
+        x = [id2title_dict[y] for y in x]
+        print(x)
+    # print(result_df[args.titles])
+    # print(result_df[args.abstracts])
+    # print(result_df[args.titles].map(id2title_dict))
+    # print(result_df[args.abstracts].map(id2abstract_dict))
--- workloads/query_gen.py
+++ workloads/query_gen.py
@@ -1,8 +1,8 @@
-'''
+"""
 This script should take in a dataframe and generate a list of queries (query text, k value)
 
 A workload should be a dataframe, and will be write to a csv file. 
-'''
+"""
 
 import sys
 import random
@@ -21,10 +21,11 @@ from keyword_extractor import *
 # random.seed(0)
 # np.random.seed(0)
 
+
 class QueryTemplate:
     def __init__(self):
         self.title = None
-        self.author =[]
+        self.author = []
         self.year = None
         self.categories = None
         self.keywords = []
@@ -32,11 +33,11 @@ class QueryTemplate:
         self.abstract = None
 
         self.infor_prob = {
-            'author': 0.5,
-            'year': 0.5,
-            'categories': 0.5,
-            'keywords': 0.5,
-            'journal': 0.5
+            "author": 0.3,
+            "year": 0.3,
+            "categories": 0.3,
+            "keywords": 1,
+            "journal": 0.3,
         }
 
     def generate_query(self, title=False):
@@ -55,15 +56,15 @@ class QueryTemplate:
     def append_info(self):
         # add info to the query based on the probability
         infor = ""
-        if random.random() < self.infor_prob['author']:
+        if random.random() < self.infor_prob["author"]:
             infor += self.author_query()
-        if random.random() < self.infor_prob['year']:
+        if random.random() < self.infor_prob["year"]:
             infor += self.year_query()
-        if random.random() < self.infor_prob['categories']:
+        if random.random() < self.infor_prob["categories"]:
             infor += self.categories_query()
-        if random.random() < self.infor_prob['keywords']:
+        if random.random() < self.infor_prob["keywords"]:
             infor += self.keywords_query()
-        if random.random() < self.infor_prob['journal']:
+        if random.random() < self.infor_prob["journal"]:
             infor += self.journal_query()
         return infor
 
@@ -74,37 +75,40 @@ class QueryTemplate:
 
     def parse_info(self, info: dict):
         # set the attributes of the class
-        self.author = parse_authors(info['authors'])
-        self.title = parse_title(info['title'])
-        self.year = int(info['update_date'].split("-")[0])
-        self.categories = parse_categories(info['categories'])
+        self.author = parse_authors(info["authors"])
+        self.title = parse_title(info["title"])
+        self.year = int(info["update_date"].split("-")[0])
+        self.categories = parse_categories(info["categories"])
 
         # currently only use the top 30 keywords
-        self.abstract = parse_abstract(info['abstract'])
-        self.keywords = remove_noise_from_keywords(extract_keywords(self.abstract, score=False)[:30])
-        self.journal = parse_journal(info['journal-ref'])
-        
+        self.abstract = parse_abstract(info["abstract"])
+        self.keywords = remove_noise_from_keywords(
+            extract_keywords(self.abstract, score=False)[:30]
+        )
+        print(self.keywords)
+        self.journal = parse_journal(info["journal-ref"])
+
     def title_query(self):
         return " titled < {} >".format(self.title)
-    
+
     def author_query(self):
         random_author = random.choice(self.author)
         return " written by < {} >".format(random_author)
-    
+
     def year_query(self):
         return " from year {}".format(self.year)
-    
+
     def categories_query(self):
         random_category = random.choice(self.categories)
         return " on < {} >".format(random_category)
-    
+
     def keywords_query(self):
         random_keyword = random.choice(self.keywords)
         return " about < {} >".format(random_keyword)
-    
+
     def journal_query(self):
         return " published at < {} >".format(self.journal)
-    
+
     def print_info(self):
         print("title       :{}".format(self.title))
         print("abstract    :{}".format(self.abstract))
@@ -113,30 +117,33 @@ class QueryTemplate:
         print("categories  :{}".format(self.categories))
         print("keywords    :{}".format(self.keywords))
         print("journal     :{}".format(self.journal))
-    
+
 
 def generate_one():
     # generate a query
-    ret_query = {'query':None, 'k':None, 'type': None}
+    ret_query = {"query": None, "k": None, "type": None}
     return ret_query
+
 
 def generate_many():
     # generate a list of queries
     pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # get a number from the command line
     parser = argparse.ArgumentParser()
-    parser.add_argument('-n', '--num', type=int, default=10, help='number of queries to generate')
+    parser.add_argument(
+        "-n", "--num", type=int, default=10, help="number of queries to generate"
+    )
     args = parser.parse_args()
     numbers_of_queries = args.num
 
-    file = open('../data/filtered_data.pickle', 'rb')
+    file = open("../data/filtered_data.pickle", "rb")
     data = pickle.load(file)
     file.close()
     data.reset_index(drop=True, inplace=True)
-    
+
     # sample 1 row from the data, print it out
     sample = data.sample(1).iloc[0]
     sample = dict(sample)
@@ -152,4 +159,4 @@ if __name__ == '__main__':
     print()
     print("<<< Randomly generated queries >>>")
     for i in range(numbers_of_queries):
-        print("{}.".format(i+1), query_template.generate_query())
+        print("{}.".format(i + 1), query_template.generate_query())
