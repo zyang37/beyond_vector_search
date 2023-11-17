ee8c13e1e036d1f484706874968c3933b1215d43
mmartin4972 <mmartin4972@gmail.com>
2023-11-17 13:23:35 -0500

    Added some helpful debugging information to the graph search algorithm

--- vector_graph/bipartite_graph_dict.py
+++ vector_graph/bipartite_graph_dict.py
@@ -82,10 +82,10 @@ class BipartiteGraphDict:
         ids_to_explore = input_ids_list
         explored_keyword_ids = set()
         round_num = 0
+        debug_node_map = {}
 
         while len(res) < k and len(ids_to_explore) != 0:
             # Get the next set of keywords to explore
-            # Track keywords that were already explored
             cur_relevant_data_ids = []
             keywords_to_explore = set()
             for cur_id in ids_to_explore:
@@ -93,10 +93,18 @@ class BipartiteGraphDict:
                     if keyword_id not in explored_keyword_ids:
                         keywords_to_explore.add(keyword_id)
 
+                        if debug:
+                            debug_node_map[keyword_id] = cur_id
+
             # Go through all of the keywords to find the resultant data ids
             for keyword_id in keywords_to_explore:
                 explored_keyword_ids.add(keyword_id)
                 cur_relevant_data_ids += self.keyword_dict[keyword_id]
+
+                if debug:
+                    for data_id in self.keyword_dict[keyword_id]:
+                        if data_id not in input_ids_list and data_id not in res:
+                            debug_node_map[data_id] = keyword_id
 
             # Sort this round of ids by the number of keywords explored in this round they are associated with
             c = Counter(cur_relevant_data_ids)
@@ -111,8 +119,20 @@ class BipartiteGraphDict:
             res += ids_to_explore
             round_num += 1
 
-        self.log(f"num rounds {round_num}")
-        return res[:k]
+        self.log(f"num search rounds: {round_num}")
+        res = res[:k]
+
+        if debug:
+            for id in res:
+                # Build the id chain
+                id_add_chain = [id]
+                cur_id_in_chain = id
+                while cur_id_in_chain in debug_node_map.keys():
+                    cur_id_in_chain = debug_node_map[cur_id_in_chain]
+                    id_add_chain.append(cur_id_in_chain)
+
+                self.log(f"{id}, discovery chain: {id_add_chain}")
+        return res
 
     def get_common_keywords_from_id_list(self, id_list) -> list[Any]:
         keyword_list = []
